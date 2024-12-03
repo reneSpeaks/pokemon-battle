14951995bd9deb9848608124bc67d96a7ffee51e
René Weiberlenn <rene.weiberlenn@live.de>
2024-12-03 13:43:36 +0100

    added pagination to landing page

--- src/sections/PokemonList.jsx
+++ src/sections/PokemonList.jsx
@@ -5,9 +5,37 @@ import PokemonCard from '../components/PokemonCard.jsx';
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState({});
+  const pokemonCount = 151;
+  const limit = 3;
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_POKE_API}/pokemon?limit=12&offset=0`)
+    const totalCount = pokemons.length;
+    const totalPages = Math.ceil(totalCount / limit);
+
+    const startingIndex = (page - 1) * limit;
+    const endingIndex = startingIndex + limit;
+
+    let result = [];
+    if (endingIndex > totalCount) {
+      result = pokemons.slice(startingIndex);
+    } else {
+      result = pokemons.slice(startingIndex, endingIndex);
+    }
+
+    setPagination({
+      totalCount,
+      totalPages,
+      currentPage: page,
+      hasNextPage: page < totalPages,
+      hasPreviousPage: page > 1,
+      results: result,
+    });
+  }, [pokemons, page]);
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_POKE_API}/pokemon?limit=${pokemonCount}&offset=0`)
     .then((response) => {
       setPokemons(response.data.results);
     }).catch((error) => {
@@ -15,11 +43,31 @@ const PokemonList = () => {
     });
   }, []);
 
+  function handlePage(value) {
+    if (value === 'inc') {
+      if (pagination.hasNextPage) {
+        setPage(prevState => prevState + 1);
+      }
+    } else if (value === 'dec') {
+      if (pagination.hasPreviousPage) {
+        setPage(prevState => prevState - 1);
+      }
+    }
+  }
+
   return (
-    <section id="pokemon-list" className="flex flex-row flex-wrap justify-center">
-      {pokemons && pokemons.map((pokemon, id) => (
-        <PokemonCard pokemonURL={pokemon.url} key={id} />
-      ))}
+    <section id="pokemon-list" className="flex flex-col justify-center items-center">
+      <div className="flex flex-row flex-wrap justify-center">
+        {pagination.results && pagination.results.map((pokemon) => (
+          <PokemonCard pokemonURL={pokemon.url} key={pokemon.name} />
+        ))}
+      </div>
+
+      <div className="join">
+        <button className="join-item btn" onClick={() => handlePage('dec')}>«</button>
+        <button className="join-item btn btn-disabled">{page}</button>
+        <button className="join-item btn" onClick={() => handlePage('inc')}>»</button>
+      </div>
     </section>
   );
 };
