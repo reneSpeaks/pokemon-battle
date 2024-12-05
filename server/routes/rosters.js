78a93ef72c5ab4ea0d320ebf87ca0e5b53f9b35e
René Weiberlenn <rene.weiberlenn@live.de>
2024-12-05 21:48:53 +0100

    integration of RosterContext into backend functionality

--- server/routes/rosters.js
+++ server/routes/rosters.js
@@ -16,7 +16,7 @@ router.route("/:id").get(getRoster).put(updateRoster).delete(deleteRoster);
 // router.route("/user/:userId").get(getRosterByUserId).put(updateRosterByUserId);;
 // http://localhost:3001/rosters/user/675029271dcee3215b59d153
 
-router.route("/by-user/:userId").get(getRosterByUserId).put(updateRosterByUserId);;
+router.route("/by-user/:userId").get(getRosterByUserId).put(updateRosterByUserId);
 // http://localhost:3001/rosters/by-user/675029271dcee3215b59d153
 
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,16 +2,15 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { FaHeart } from 'react-icons/fa';
-import { isStored } from '../utils/storage.js';
 import { capitalize } from '../utils/helper.js';
 import { useRoster } from '../contexts/RosterContext.jsx';
 import { useUser } from '../contexts/UserContext.jsx';
 
 const PokemonCard = ({ pokemonURL }) => {
   const { user } = useUser()
-  const { addToRoster, removeFromRoster } = useRoster();
+  const { roster, addToRoster, removeFromRoster, isRoster } = useRoster();
   const [pokemon, setPokemon] = useState(null);
-  const [isRoster, setIsRoster] = useState(false);
+  const [isHeart, setIsHeart] = useState(false);
   const typeColor = {
     bug: "#26de81",
     dragon: "#ffeaa7",
@@ -35,21 +34,26 @@ const PokemonCard = ({ pokemonURL }) => {
     axios.get(pokemonURL)
     .then((response) => {
       setPokemon(response.data);
-      setIsRoster(() => isStored(import.meta.env.VITE_ROSTERSTORAGE, { id: response.data.id }));
     })
     .catch((error) => {
       toast.error("Error catching Pokemon.", error);
     });
   }, []);
 
+  useEffect(() => {
+    if (pokemon && roster) {
+      setIsHeart(roster.some((item) => item.id === pokemon.id));
+    }
+  }, [pokemon, roster])
+
   const handleClick = () => {
     if (user.length > 0) {
-      if (!isRoster && pokemon) {
+      if (!isHeart && pokemon) {
         addToRoster(pokemon);
-        setIsRoster(() => isStored(import.meta.env.VITE_ROSTERSTORAGE, { id: pokemon.id }));
-      } else if (isRoster && pokemon) {
+        setIsHeart(isRoster({ id: pokemon.id }));
+      } else if (isHeart && pokemon) {
         removeFromRoster(pokemon);
-        setIsRoster(() => isStored(import.meta.env.VITE_ROSTERSTORAGE, { id: pokemon.id }));
+        setIsHeart(isRoster({ id: pokemon.id }));
       } else {
         toast.error("Something went wrong.");
       }
@@ -61,7 +65,7 @@ const PokemonCard = ({ pokemonURL }) => {
       {pokemon && (
         <div id="pokemon-card" onClick={handleClick} className="m-4 max-w-[350px] w-full h-[450px] px-8 py-5 shadow-xl rounded-2xl cursor-pointer hover:shadow-lg hover:shadow-accent hover:scale-105 duration-200" style={{ background: `radial-gradient(circle at 50% 0%, ${typeColor[pokemon.types[0].type.name]} 36%, #ffffff 36%)` }}>
           <div className="flex flex-row">
-            {user.length > 0 && <FaHeart className={isRoster ? "text-red-700 drop-shadow-lg" : "text-white drop-shadow-lg"} />}
+            {user.length > 0 && <FaHeart className={isHeart ? "text-red-700 drop-shadow-lg" : "text-white drop-shadow-lg"} />}
             <p className="pokemon-hp w-20 bg-white text-center px-2 py-0 rounded-xl font-normal ml-auto">
               <span className="text-xs tracking-wider font-semibold">HP</span> {pokemon.stats[0].base_stat}
             </p>
