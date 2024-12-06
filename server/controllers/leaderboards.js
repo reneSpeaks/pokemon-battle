f79dd7d4d6afd44d4290edaab803a6c0b8f8bb89
René Weiberlenn <rene.weiberlenn@live.de>
2024-12-06 15:33:03 +0100

    redesign leaderboard, fixing username not found, adding battle functionaltiy

--- server/controllers/leaderboards.js
+++ server/controllers/leaderboards.js
@@ -1,50 +1,50 @@
 import Leaderboard from "../models/leaderboard.js";
 
 export const getLeaderboards = async (req, res) => {
-    try {
-      const leaderboards = await Leaderboard.find({})
-        .populate({
-          path: "userId", // The field in Leaderboard referencing User
-          select: "username firstName lastName", // Fields to include from User
-        }).sort({ score: -1 }).limit(10);
-  
-      res.status(200).json(leaderboards);
-    } catch (error) {
-      console.error("Error fetching leaderboards:", error);
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  };;
+  try {
+    const leaderboards = await Leaderboard.find({})
+    .populate({
+      path: "userId", // The field in Leaderboard referencing User
+      select: "username firstName lastName", // Fields to include from User
+    }).sort({ score: -1 }).limit(10);
 
-  export const getLeaderboard = async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const leaderboard = await Leaderboard.findById(id)
-        .populate({
-          path: "userId", // The field in Leaderboard referencing User
-          select: "username firstName lastName", // Fields to include from User
-        });
-  
-      if (!leaderboard) {
-        return res.status(404).json({ message: "Leaderboard entry not found" });
-      }
-  
-      res.status(200).json(leaderboard);
-    } catch (error) {
-      console.error("Error fetching leaderboard:", error);
-      res.status(500).json({ message: "Something went wrong" });
+    res.status(200).json(leaderboards);
+  } catch (error) {
+    console.error("Error fetching leaderboards:", error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const getLeaderboard = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const leaderboard = await Leaderboard.findOne({ userId: id })
+    .populate({
+      path: "userId", // The field in Leaderboard referencing User
+      select: "username firstName lastName", // Fields to include from User
+    });
+
+    if (!leaderboard) {
+      return res.status(404).json({ message: "Leaderboard entry not found" });
     }
-  };
+
+    res.status(200).json(leaderboard);
+  } catch (error) {
+    console.error("Error fetching leaderboard:", error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 export const createLeaderboard = async (req, res) => {
-    try {
-        const { userId, battlesWon, battlesLost, battlesDraw,score } = req.body;
-        const leaderboard = await Leaderboard.create({ userId, battlesWon, battlesLost, battlesDraw,score });
-        res.status(201).json(leaderboard);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-    }
+  try {
+    const { userId, battlesWon, battlesLost, battlesDraw, score } = req.body;
+    const leaderboard = await Leaderboard.create({ userId, battlesWon, battlesLost, battlesDraw, score });
+    res.status(201).json(leaderboard);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 // Update Score after a Battle Create a form or function to send the score update request to the backend.Update Score after a Battle Create a form or function to send the score update request to the backend.
 // Update score after a battle
@@ -70,28 +70,28 @@ export const createLeaderboard = async (req, res) => {
 //   }
 // };
 export const updateLeaderboard = async (req, res) => {
-    try {
-        const {
-            params: { id },
-            body,
-        } = req;
+  try {
+    const {
+      params: { id },
+      body,
+    } = req;
 
-        const leaderboard = await Leaderboard.findByIdAndUpdate(id, body, { new: true });
+    const leaderboard = await Leaderboard.findOneAndUpdate({ userId: id }, body, { new: true });
 
-        res.status(200).json(leaderboard);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-    }
+    res.status(200).json(leaderboard);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const deleteLeaderboard = async (req, res) => {
-    try {
-        const { id } = req.params;
-        await Leaderboard.findByIdAndDelete(id);
-        res.status(204).json({ message: "Leaderboard deleted successfully" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-    }
+  try {
+    const { id } = req.params;
+    await Leaderboard.findByIdAndDelete(id);
+    res.status(204).json({ message: "Leaderboard deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
